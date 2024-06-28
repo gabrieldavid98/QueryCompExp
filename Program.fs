7fb6451021c46bf696ec3aa754e9ffb71bc070af
Gabriel Mendoza <gabrieldavid_98@hotmail.com>
2024-06-27 22:41:15 -0500

    initial commit

---
+++ Program.fs
@@ -0,0 +1,33 @@
+﻿open FSharp.Data
+
+[<Literal>]
+let postsUrl = "https://jsonplaceholder.typicode.com/posts"
+
+[<Literal>]
+let commentsUrl = "https://jsonplaceholder.typicode.com/comments"
+
+type Posts = JsonProvider<postsUrl>
+type Comments = JsonProvider<commentsUrl>
+
+let posts = Posts.Load(postsUrl)
+let comments = Comments.Load(commentsUrl)
+
+let lastPost id =
+    query {
+        for post in posts do
+            where (post.Id = id)
+            groupJoin comment in comments on (post.Id = comment.PostId) into comments
+
+            select (
+                post,
+                query {
+                    for comment in comments do
+                        take 2
+                        select comment
+                }
+            )
+
+            exactlyOne
+    }
+
+printfn "%A" (lastPost 7)
